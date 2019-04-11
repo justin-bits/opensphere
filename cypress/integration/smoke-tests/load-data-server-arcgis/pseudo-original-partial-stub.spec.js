8ad2d074b6e561a6fdcde37a901d3ffc47214406
Justin Denton <justin.denton@caci.com>
2019-04-11 13:53:53 -0600

    test(cypress): Double request solution attempts, dirty commit, for historical only

---
+++ cypress/integration/smoke-tests/load-data-server-arcgis/pseudo-original-partial-stub.spec.js
@@ -0,0 +1,110 @@
+/// <reference types="Cypress" />
+var os = require('../../../support/selectors.js');
+
+describe('Add ARCGIS server', function() {
+  before('Login', function() {
+    cy.login();
+    cy.server();
+
+    cy.route('**/OpenData/MapServer', 'fx:/smoke-tests/load-data-server-arcgis/mapserver.stub.xml')
+        .as('Connect to server');
+    cy.route('**/OpenData/MapServer?f=json', 'fx:/smoke-tests/load-data-server-arcgis/mapserver?f=json.stub.json')
+        .as('Get layers');
+    cy.route('**/OpenData/MapServer/layers?f=json', 'fx:/smoke-tests/load-data-server-arcgis/layers?f=json.stub.json')
+        .as('Get layer details');
+    cy.route('**/OpenData/MapServer/export?F=image*', 'fx:/smoke-tests/load-data-server-arcgis/export.png')
+        .as('Turn on layer');
+  });
+
+  it('Load data from ARCGIS server', function() {
+    // Add a server
+    cy.get(os.statusBar.SERVERS_BUTTON).click();
+    cy.get(os.settingsDialog.Tabs.dataServers.ADD_SERVER_BUTTON).click();
+    cy.get(os.importURLDialog.ENTER_A_URL_INPUT)
+        .type('https://ags.auroragov.org/aurora/rest/services/OpenData/MapServer');
+    cy.get(os.importURLDialog.NEXT_BUTTON).click();
+    cy.get(os.addArcServerDialog.TITLE_INPUT).clear();
+    cy.get(os.addArcServerDialog.TITLE_INPUT).type('Aurora ArcGIS Server');
+    cy.get(os.addArcServerDialog.SAVE_BUTTON).click();
+    cy.wait('@Get layer details'); // Large file, times out without a wait
+    cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+        .should('contain', 'Aurora ArcGIS Server');
+    cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+        .find(os.settingsDialog.Tabs.dataServers.SERVER_ONLINE_BADGE_WILDCARD)
+        .should('be.visible');
+    cy.get(os.settingsDialog.DIALOG_CLOSE).click();
+
+    // Load a layer
+    cy.get(os.Toolbar.addData.BUTTON).click();
+    cy.get(os.addDataDialog.SEARCH_INPUT).type('fire station');
+    cy.get(os.addDataDialog.Tree.LAYER_1).should('contain', 'Fire Stations');
+    cy.get(os.addDataDialog.Tree.LAYER_1)
+        .find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD)
+        .should('have.class', os.addDataDialog.Tree.LAYER_IS_OFF_CLASS_WILDCARD);
+    cy.get(os.addDataDialog.Tree.LAYER_1).find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD).click();
+    cy.get(os.addDataDialog.Tree.LAYER_1)
+        .find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD)
+        .should('have.class', os.addDataDialog.Tree.LAYER_IS_ON_CLASS_WILDCARD);
+    cy.get(os.addDataDialog.CLOSE_BUTTON).click();
+    cy.get(os.addDataDialog.DIALOG).should('not.exist');
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('contain', 'Fire Stations Features (0)');
+
+    // Import and activate a query area
+    cy.get(os.layersDialog.Tabs.Areas.TAB).click();
+    cy.get(os.layersDialog.Tabs.Areas.Import.BUTTON).click();
+    cy.upload('smoke-tests/load-data-server-arcgis/test-area.geojson');
+    cy.get(os.importDataDialog.NEXT_BUTTON).click();
+    cy.get(os.geoJSONAreaImportDialog.Tabs.areaOptions.TITLE_COLUMN_INPUT).should('be.visible');
+    cy.get(os.geoJSONAreaImportDialog.DONE_BUTTON).click();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).should('contain', 'temp area 5');
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).rightClick();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.contextMenu.menuOptions.ZOOM).click();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).rightClick();
+
+    // THIS TEST PASSES, USE TO VIEW REQUESTS AS THEY ARE EXPECTED TO WORK
+
+     // THIS LINE CAUSES TWO REQUESTS TO BE SENT
+    cy.get(os.layersDialog.Tabs.Areas.Tree.contextMenu.menuOptions.Query.LOAD).click();
+
+    // CHECK TO SEE IF IT WORKED
+    cy.get(os.layersDialog.Tabs.Layers.TAB).click();
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('contain', 'Fire Station');
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+        .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+        .should('not.contain', 'Loading...'); // wait for feature count value to stabilize
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+        .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+        .should('not.contain', '(0)'); // wait for feature count value to stabilize
+
+    // ORIGINAL TEST CODE, CAN BE ENABLED AFTER THIS IS WORKING
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+    //     .invoke('text')
+    //     .should('match', /\([1-9]\d{0,3}\)/); // Any number 1-9999, surrounded by ()
+
+    // // Clean up
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+    //     .invoke('text')
+    //     .should('match', /\([1-9]\d{0,3}\)/); // Any number 1-9999, surrounded by ()
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).click();
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.REMOVE_LAYER_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('not.contain', 'Fire Stations Features');
+    // cy.get(os.layersDialog.Tabs.Areas.TAB).click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1)
+    //     .find(os.layersDialog.Tabs.Areas.Tree.REMOVE_AREA_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).should('not.contain', 'temp area 5');
+    // cy.get(os.layersDialog.Tabs.Layers.TAB).click();
+    // cy.get(os.statusBar.SERVERS_BUTTON).click();
+    // cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+    //     .find(os.settingsDialog.Tabs.dataServers.DELETE_SERVER_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+    //     .should('not.contain', 'Aurora ArcGIS Server');
+    // cy.get(os.settingsDialog.CLOSE_BUTTON).click();
+  });
+});
---
+++ cypress/integration/smoke-tests/load-data-server-arcgis/server-onrequest-response.spec.js
@@ -0,0 +1,132 @@
+/// <reference types="Cypress" />
+var os = require('../../../support/selectors.js');
+
+describe('Add ARCGIS server', function() {
+  before('Login', function() {
+    cy.login();
+
+    // *********** START TEST CODE ***********
+    // REQUESTS ARE ABORTED WHEN USING ONREQUEST (VS ONRESPONSE)
+    var query1;
+    var query2;
+    cy.fixture('/smoke-tests/load-data-server-arcgis/query-1.stub.json').as('getFeatureList').then(function(query) {
+      query1 = query;
+    });
+    cy.fixture('/smoke-tests/load-data-server-arcgis/query-2.stub.json').as('getFeatureDetails').then(function(query) {
+      query2 = query;
+    });
+
+    cy.server({
+      onRequest: function(xhr) {
+        if ((xhr.request.body).includes('geometryType')) {
+          xhr.response.body = query1;
+        } else if ((xhr.request.body).includes('objectIds')) {
+          xhr.response.body = query2;
+        }
+      }
+    });
+
+    cy.route('POST', '**/OpenData/MapServer/5/query', 'fx:/smoke-tests/load-data-server-arcgis/dummy.stub.txt')
+        .as('dummy'); // WITHOUT THIS LINE, REQUESTS ARE NEVER ROUTED THROUGH CY.SERVER
+    // *********** END TEST CODE ***********
+
+    cy.route('**/OpenData/MapServer', 'fx:/smoke-tests/load-data-server-arcgis/mapserver.stub.xml')
+        .as('Connect to server');
+    cy.route('**/OpenData/MapServer?f=json', 'fx:/smoke-tests/load-data-server-arcgis/mapserver?f=json.stub.json')
+        .as('Get layers');
+    cy.route('**/OpenData/MapServer/layers?f=json', 'fx:/smoke-tests/load-data-server-arcgis/layers?f=json.stub.json')
+        .as('Get layer details');
+    cy.route('**/OpenData/MapServer/export?F=image*', 'fx:/smoke-tests/load-data-server-arcgis/export.png')
+        .as('Turn on layer');
+  });
+
+  it('Load data from ARCGIS server', function() {
+    // Add a server
+    cy.get(os.statusBar.SERVERS_BUTTON).click();
+    cy.get(os.settingsDialog.Tabs.dataServers.ADD_SERVER_BUTTON).click();
+    cy.get(os.importURLDialog.ENTER_A_URL_INPUT)
+        .type('https://ags.auroragov.org/aurora/rest/services/OpenData/MapServer');
+    cy.get(os.importURLDialog.NEXT_BUTTON).click();
+    cy.get(os.addArcServerDialog.TITLE_INPUT).clear();
+    cy.get(os.addArcServerDialog.TITLE_INPUT).type('Aurora ArcGIS Server');
+    cy.get(os.addArcServerDialog.SAVE_BUTTON).click();
+    cy.wait('@Get layer details'); // Large file, times out without a wait
+    cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+        .should('contain', 'Aurora ArcGIS Server');
+    cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+        .find(os.settingsDialog.Tabs.dataServers.SERVER_ONLINE_BADGE_WILDCARD)
+        .should('be.visible');
+    cy.get(os.settingsDialog.DIALOG_CLOSE).click();
+
+    // Load a layer
+    cy.get(os.Toolbar.addData.BUTTON).click();
+    cy.get(os.addDataDialog.SEARCH_INPUT).type('fire station');
+    cy.get(os.addDataDialog.Tree.LAYER_1).should('contain', 'Fire Stations');
+    cy.get(os.addDataDialog.Tree.LAYER_1)
+        .find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD)
+        .should('have.class', os.addDataDialog.Tree.LAYER_IS_OFF_CLASS_WILDCARD);
+    cy.get(os.addDataDialog.Tree.LAYER_1).find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD).click();
+    cy.get(os.addDataDialog.Tree.LAYER_1)
+        .find(os.addDataDialog.Tree.LAYER_TOGGLE_SWITCH_WILDCARD)
+        .should('have.class', os.addDataDialog.Tree.LAYER_IS_ON_CLASS_WILDCARD);
+    cy.get(os.addDataDialog.CLOSE_BUTTON).click();
+    cy.get(os.addDataDialog.DIALOG).should('not.exist');
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('contain', 'Fire Stations Features (0)');
+
+    // Import and activate a query area
+    cy.get(os.layersDialog.Tabs.Areas.TAB).click();
+    cy.get(os.layersDialog.Tabs.Areas.Import.BUTTON).click();
+    cy.upload('smoke-tests/load-data-server-arcgis/test-area.geojson');
+    cy.get(os.importDataDialog.NEXT_BUTTON).click();
+    cy.get(os.geoJSONAreaImportDialog.Tabs.areaOptions.TITLE_COLUMN_INPUT).should('be.visible');
+    cy.get(os.geoJSONAreaImportDialog.DONE_BUTTON).click();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).should('contain', 'temp area 5');
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).rightClick();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.contextMenu.menuOptions.ZOOM).click();
+    cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).rightClick();
+
+     // THIS LINE CAUSES TWO REQUESTS TO BE SENT
+    cy.get(os.layersDialog.Tabs.Areas.Tree.contextMenu.menuOptions.Query.LOAD).click();
+
+    // CHECK TO SEE IF IT WORKED
+    cy.get(os.layersDialog.Tabs.Layers.TAB).click();
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('contain', 'Fire Station');
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+        .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+        .should('not.contain', 'Loading...'); // wait for feature count value to stabilize
+    cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+        .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+        .should('not.contain', '(0)'); // wait for feature count value to stabilize
+
+    // ORIGINAL TEST CODE, CAN BE ENABLED AFTER THIS IS WORKING
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+    //     .invoke('text')
+    //     .should('match', /\([1-9]\d{0,3}\)/); // Any number 1-9999, surrounded by ()
+
+    // // Clean up
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.FEATURE_COUNT_TEXT_WILDCARD)
+    //     .invoke('text')
+    //     .should('match', /\([1-9]\d{0,3}\)/); // Any number 1-9999, surrounded by ()
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).click();
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4)
+    //     .find(os.layersDialog.Tabs.Layers.Tree.Type.featureLayer.REMOVE_LAYER_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.layersDialog.Tabs.Layers.Tree.LAYER_4).should('not.contain', 'Fire Stations Features');
+    // cy.get(os.layersDialog.Tabs.Areas.TAB).click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1)
+    //     .find(os.layersDialog.Tabs.Areas.Tree.REMOVE_AREA_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.layersDialog.Tabs.Areas.Tree.AREA_1).should('not.contain', 'temp area 5');
+    // cy.get(os.layersDialog.Tabs.Layers.TAB).click();
+    // cy.get(os.statusBar.SERVERS_BUTTON).click();
+    // cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+    //     .find(os.settingsDialog.Tabs.dataServers.DELETE_SERVER_BUTTON_WILDCARD)
+    //     .click();
+    // cy.get(os.settingsDialog.Tabs.dataServers.SERVER_1)
+    //     .should('not.contain', 'Aurora ArcGIS Server');
+    // cy.get(os.settingsDialog.CLOSE_BUTTON).click();
+  });
+});
